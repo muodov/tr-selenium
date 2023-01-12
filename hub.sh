c5a75d712e0727e92fd0b3f23be132838a857035
Maxim Tsoy <maks.tsoy@gmail.com>
2023-01-12 22:47:09 +0000

    second commit

---
+++ hub.sh
@@ -0,0 +1,2 @@
+docker run -d -p 4442-4444:4442-4444 --name selenium-hub selenium/hub:4.5.3-20221024
+
---
+++ node.sh
@@ -0,0 +1,9 @@
+docker run -d -p 5555:5555 \
+    -e SE_EVENT_BUS_HOST=10.5.0.5 \
+    -e SE_EVENT_BUS_PUBLISH_PORT=4442 \
+    -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 \
+    -v ${PWD}/config.toml:/opt/bin/config.toml \
+    -v ${PWD}/assets:/opt/selenium/assets \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    --name selenium-node \
+    selenium/node-docker:4.5.3-20221024
