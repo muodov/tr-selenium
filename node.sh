66ee2cd8f16ae82b6be47d7b31beb1878a5cd684
Maxim Tsoy <mtsoy@duckduckgo.com>
2023-04-14 07:14:18 -0400

    tweak

--- node.sh
+++ node.sh
@@ -1,9 +1,10 @@
+export HUB_IP=10.100.9.22
 docker run --rm -p 5555:5555 \
-    -e SE_EVENT_BUS_HOST=10.5.0.5 \
+    -e SE_EVENT_BUS_HOST=${HUB_IP} \
     -e SE_EVENT_BUS_PUBLISH_PORT=4442 \
     -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 \
     -v ${PWD}/config.toml:/opt/bin/config.toml \
-    -v ${PWD}/assets:/opt/selenium/assets \
+    -v /dev/shm/selenium-assets:/opt/selenium/assets \
     -v /var/run/docker.sock:/var/run/docker.sock \
     --name selenium-node \
     selenium/node-docker:4.8.0-20230131
