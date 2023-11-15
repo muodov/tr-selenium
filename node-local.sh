4cb3ec8fa6b537782baa626ef173bce938d9568c
Maxim Tsoy <mtsoy@duckduckgo.com>
2023-11-15 07:24:14 -0500

    tweak node-local

--- node-local.sh
+++ node-local.sh
@@ -5,10 +5,11 @@ export THIS_NODE_IP=10.100.9.21
 # -v /var/run/docker.sock:/var/run/docker.sock \
 
 docker run --rm -p 5555:5555 \
-    --shm-size="2g" \
+    --shm-size="50g" \
     -e SE_EVENT_BUS_HOST=${HUB_IP} \
     -e SE_EVENT_BUS_PUBLISH_PORT=4442 \
     -e SE_EVENT_BUS_SUBSCRIBE_PORT=4443 \
     -e SE_NODE_HOST=${THIS_NODE_IP} \
-    --name selenium-node \
+    -e SE_NODE_MAX_SESSIONS=30 \
+    -e SE_NODE_OVERRIDE_MAX_SESSIONS=true \
     selenium/node-chrome:4.15.0-20231110
